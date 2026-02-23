aa38da84191f4403a64e329636c3aa99ebef1758
szymg2001 <szymon.grabowiec2001@gmail.com>
2026-02-23 12:38:16 +0100

    SIK-5 Utworzenie endpointu do pobierania danych

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const fs = require("fs");
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Serwer działa");
+});
+
+app.get("/products", (req, res) => {
+  fs.readFile("../data.json", (err, data) => {
+    if (err) throw err;
+
+    res.send(data);
+  });
 });
 
 app.listen(PORT, () => {
