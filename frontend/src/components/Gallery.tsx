ce05398927dade00ff6d4578112a52c2cfe199bc
szymg2001 <szymon.grabowiec2001@gmail.com>
2026-02-24 15:31:43 +0100

    SIK-9 Filtrowanie katalogu

--- frontend/src/components/Gallery.tsx
+++ frontend/src/components/Gallery.tsx
@@ -3,11 +3,30 @@ import "../css/gallery.css";
 import type { Product } from "../App";
 
 export default function Gallery({ items }: { items: Product[] }) {
+  const [sortOrder, setSortOrder] = React.useState<"asc" | "desc" | null>(null);
+  const sortedItems = React.useMemo(() => {
+    if (!sortOrder) return items;
+
+    return [...items].sort((a, b) => {
+      if (sortOrder === "asc") return a.price - b.price;
+      return b.price - a.price;
+    });
+  }, [items, sortOrder]);
+
   return (
     <div className="gallery">
-      {items.map((item) => (
-        <GalleryItem {...item} key={item.id} />
-      ))}
+      <div className="gallery__filters">
+        <button onClick={() => setSortOrder("asc")}>Cena: od najniższej</button>
+        <button onClick={() => setSortOrder("desc")}>
+          Cena: od najwyższej
+        </button>
+        <button onClick={() => setSortOrder(null)}>Resetuj</button>
+      </div>
+      <div className="gallery-content">
+        {sortedItems.map((item) => (
+          <GalleryItem {...item} key={item.id} />
+        ))}
+      </div>
     </div>
   );
 }
